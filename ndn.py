a81e094602b5bdd4752235ead86cc2cd25460a59
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-06-22 17:51:55 +0200

    port ndn.py to python3 and add static typing

--- ndn.py
+++ ndn.py
@@ -11,17 +11,17 @@ import random
 
 class Roll:
 	def __init__(self,amount,size,modifier):
-		self.amount = amount
-		self.size = size
-		self.modifier = modifier
+		self.amount:int = amount
+		self.size:int = size
+		self.modifier:int = modifier
 
-	def roll(self):
-		values = []
-		for i in range(0,self.amount):
-			randvalue = random.randint(1,self.size)
+	def roll(self) -> int:
+		values :list[int] = []
+		for _ in range(0,self.amount):
+			randvalue:int = random.randint(1,self.size)
 			values.append(randvalue)
 		return sum(values) + self.modifier
 
 if __name__ == "__main__":
 	roll1d20 = Roll(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
-	print roll1d20.roll()
+	print(roll1d20.roll())
