a2010400f01d15a9e4ae8f684b3a07fc49d5b8fe
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-06-22 18:07:50 +0200

    add advantage and disadvantage to ndn.py

--- ndn.py
+++ ndn.py
@@ -10,18 +10,30 @@ import sys
 import random
 
 class Roll:
-	def __init__(self,amount,size,modifier):
+	def __init__(self,amount,size,modifier,advantage):
 		self.amount:int = amount
 		self.size:int = size
 		self.modifier:int = modifier
+		self.advantage:str = advantage
 
 	def roll(self) -> int:
 		values :list[int] = []
-		for _ in range(0,self.amount):
-			randvalue:int = random.randint(1,self.size)
-			values.append(randvalue)
+		if self.advantage == "a":
+			for _ in range(self.amount):
+				roll1 = random.randint(0,self.size)
+				roll2 = random.randint(0,self.size)
+				values.append(max(roll1,roll2))
+		elif self.advantage == "d":
+			for _ in range(self.amount):
+				roll1 = random.randint(0,self.size)
+				roll2 = random.randint(0,self.size)
+				values.append(min(roll1,roll2))
+		else:
+			for _ in range(self.amount):
+				randvalue:int = random.randint(1,self.size)
+				values.append(randvalue)
 		return sum(values) + self.modifier
 
 if __name__ == "__main__":
-	roll1d20 = Roll(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
+	roll1d20 = Roll(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]), str(sys.argv[4]))
 	print(roll1d20.roll())
