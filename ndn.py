9443393c81ab9b80a9db2a0870d4b123edac722e
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-06-24 17:41:42 +0200

    refactor ndn.py

--- ndn.py
+++ ndn.py
@@ -10,7 +10,7 @@ import sys
 import random
 
 class Roll:
-	def __init__(self,amount,size,modifier,advantage):
+	def __init__(self,amount,size,modifier,advantage=""):
 		self.amount:int = amount
 		self.size:int = size
 		self.modifier:int = modifier
@@ -20,17 +20,17 @@ class Roll:
 		values :list[int] = []
 		if self.advantage == "a":
 			for _ in range(self.amount):
-				roll1 = random.randint(0,self.size)
-				roll2 = random.randint(0,self.size)
+				roll1 :int = random.randint(0,self.size)
+				roll2 :int = random.randint(0,self.size)
 				values.append(max(roll1,roll2))
 		elif self.advantage == "d":
 			for _ in range(self.amount):
-				roll1 = random.randint(0,self.size)
-				roll2 = random.randint(0,self.size)
+				roll1 :int = random.randint(0,self.size)
+				roll2 :int = random.randint(0,self.size)
 				values.append(min(roll1,roll2))
 		else:
 			for _ in range(self.amount):
-				randvalue:int = random.randint(1,self.size)
+				randvalue :int = random.randint(1,self.size)
 				values.append(randvalue)
 		return sum(values) + self.modifier
 
